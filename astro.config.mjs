94c842174601e3adaa8ac86e43941c5d51f544da
alisyaifudin <muhammad.ali.syaifudin@hotmail.com>
2024-05-17 18:34:02 +0700

    feat: mdx & katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "monokai"
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+    gfm: true
+  }), tailwind()]
+});
