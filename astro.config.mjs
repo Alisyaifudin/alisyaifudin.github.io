46eee364624220a723431e4b5342421b1820df32
alisyaifudin <muhammad.ali.syaifudin@hotmail.com>
2024-05-17 18:36:25 +0700

    feat: github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alisyaifudin.github.io',
   integrations: [mdx({
     syntaxHighlight: "shiki",
     shikiConfig: {
